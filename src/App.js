6052cfb17e5b5588c4507b92d8ea132d0d5f5ad9
Jakub Chabrowski <chabrowski.kuba@gmail.com>
2020-03-18 15:10:07 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <React.Fragment>
+    <div className="col d-flex justify-content-center text-center">
+      <ul class="list-unstyled">
+        <li class="h2 pt-3">Drugi oddech</li>
+        <li class="h5 py-3">Aby rozpocząć, prosimy wykonać pomiar długości wydechu.</li>
+        <button type="button" class="btn btn-primary btn-lg">Wykonaj pomiar</button>
+      </ul>
     </div>
+
+    <div className="col d-flex justify-content-center text-center">
+      <ul class="list-unstyled">
+        <p class="pt-3">Jeżeli pomiar został poprzednio wykonany może Pan/i przejść bezpośrednio do ćwiczeń</p>
+        <button type="button" class="btn btn-primary btn-lg">Ćwiczenia</button>
+      </ul>
+    </div>
+    </React.Fragment>
   );
 }
 
