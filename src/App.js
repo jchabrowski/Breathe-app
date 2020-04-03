8dd6a407bbbe1e4fc7dc1db854e10a1c5076d5aa
Jakub Chabrowski <chabrowski.kuba@gmail.com>
2020-04-03 18:52:27 +0200

    switching to hash router

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import LengthTest from './components/LengthTest/LengthTest'
 import StartTest from './components/LengthTest/StartTest'
 import Exercise from './components/Exercise/Exercise';
 import Info from './components/Info/Info';
-import {  BrowserRouter, Route, Switch } from 'react-router-dom';
+import {  HashRouter, Route } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 
 import './App.css';
 
 function App() {
   return (
-  <BrowserRouter>
+  <HashRouter>
     <AnimatePresence>
         <Route exact path="/" component={Home}/>
         <Route path="/LengthTest" component={LengthTest}/>
@@ -20,7 +20,7 @@ function App() {
         <Route path="/Exercise" component={Exercise}/>    
         <Route path="/Info" component={Info}/>
     </AnimatePresence>
-  </BrowserRouter>
+  </HashRouter>
   );
 }
 
