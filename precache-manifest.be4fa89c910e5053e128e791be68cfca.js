0c9fd5b12d7f0bddd489ead6d28b47da75dbbfa3
Jakub Chabrowski <chabrowski.kuba@gmail.com>
2020-04-03 18:26:11 +0200

    Updates

---
+++ precache-manifest.be4fa89c910e5053e128e791be68cfca.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68ffbc6c8f8116dbbb49e08100525af",
+    "url": "/Breathe-app/index.html"
+  },
+  {
+    "revision": "d3db55a8890265ee9cc5",
+    "url": "/Breathe-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d37ef94e21c3d8aa0b2d",
+    "url": "/Breathe-app/static/css/main.d585d8ac.chunk.css"
+  },
+  {
+    "revision": "d3db55a8890265ee9cc5",
+    "url": "/Breathe-app/static/js/2.7c504068.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/Breathe-app/static/js/2.7c504068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d37ef94e21c3d8aa0b2d",
+    "url": "/Breathe-app/static/js/main.17a52cb6.chunk.js"
+  },
+  {
+    "revision": "f093678bb22c5af2a636",
+    "url": "/Breathe-app/static/js/runtime-main.6d67d853.js"
+  },
+  {
+    "revision": "0b465aceea91547befc40ea578c85e8b",
+    "url": "/Breathe-app/static/media/Circle.0b465ace.svg"
+  },
+  {
+    "revision": "d4c9e566a074f67a61d4dc5c9d9ab904",
+    "url": "/Breathe-app/static/media/background.d4c9e566.svg"
+  },
+  {
+    "revision": "fd40061bd21c47d1bfcd6d9747e7a7b7",
+    "url": "/Breathe-app/static/media/info.fd40061b.svg"
+  },
+  {
+    "revision": "43d6cc86765d37a9b46305b6f54d3e26",
+    "url": "/Breathe-app/static/media/logo.43d6cc86.svg"
+  },
+  {
+    "revision": "d1a0fc42eae3b00f24bebe4adcdc5626",
+    "url": "/Breathe-app/static/media/menu.d1a0fc42.svg"
+  },
+  {
+    "revision": "8a0e3abc08f5720ee6125d6d2d3d36b8",
+    "url": "/Breathe-app/static/media/test.8a0e3abc.svg"
+  },
+  {
+    "revision": "9a290468d4ca9dc67962b4e616f171bf",
+    "url": "/Breathe-app/static/media/trening.9a290468.svg"
+  }
+]);
