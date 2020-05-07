256df0010b0f1875fce5ecb0ae13e20c1a780649
Jakub Chabrowski <chabrowski.kuba@gmail.com>
2020-05-07 14:19:53 +0200

    added breathing icons

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -5,8 +5,11 @@ import { Link  } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 import { motion } from 'framer-motion';
 import nose  from '../../assets/images/nose.svg';
-import rightArrow from '../../assets/images/right-arrow.svg'
-import leftArrow from '../../assets/images/left-arrow.svg'
+import rightArrow from '../../assets/images/right-arrow.svg';
+import leftArrow from '../../assets/images/left-arrow.svg';
+import leftDownArrow from '../../assets/images/left-down-arrow.svg';
+import rightDownArrow from '../../assets/images/right-down-arrow.svg';
+import pause from '../../assets/images/pause.svg';
 
 const Exercise = () => {
 
@@ -15,15 +18,58 @@ const Exercise = () => {
       <Row className="mx-auto">
         <Col
           className="text-center my-5 mx-auto"
-          xs= { 12 }
+          xs= { 6 }
           md={{span: 6, offset: 3}}
           lg={{span: 6, offset: 3}}>
             <motion.div className="container d-flex justify-content-center">
-              <img src={nose} alt="logo"></img>
+              <img src={nose} alt="nose"></img>
             </motion.div>
-            <motion.div className="container py-2 d-flex justify-content-around px-0">
-              <img src={leftArrow} alt="left-arrow"></img> <img src={rightArrow} alt="right-arrow"></img>
+           
+            <motion.div className="container py-2 d-flex justify-content-start px-0"
+              animate={{x:[0, 10, 0],  y:[0, -10, 0]}}
+              transition={{ duration:2, loop: 4}}
+              initial={{opacity: 1}}
+              >
+              <img src={leftArrow} alt="left-arrow"></img>
             </motion.div>
+            <motion.div
+              animate={{opacity: [1, 0.4, 1]}}
+              transition={{ease: "easeInOut", duration:2, loop: 4}}
+            >
+              <Paragraph>Wdech...</Paragraph>
+            </motion.div>
+
+            <motion.div className="container py-2 d-flex justify-content-end px-0"
+              animate={{x:[0, -10, 0],  y:[0, -10, 0]}}
+              transition={{ duration:2, loop: 4}}
+              initial={{opacity: 1}}
+              >
+              <img src={rightArrow} alt="right-arrow"></img>
+            </motion.div>
+
+            <motion.div className="container py-2 d-flex justify-content-start px-0"
+              animate={{x:[0, -10, 0],  y:[0, 10, 0]}}
+              transition={{ duration:2, loop: 4}}
+              initial={{opacity: 1}}
+              >
+              <img src={leftDownArrow} alt="left-down-arrow"></img>
+            </motion.div>
+
+            <motion.div className="container py-2 d-flex justify-content-end px-0"
+              animate={{x:[0, 10, 0],  y:[0, 10, 0]}}
+              transition={{ duration:2, loop: 4}}
+              initial={{opacity: 1}}
+              >
+              <img src={rightDownArrow} alt="right-down-arrow"></img>
+            </motion.div>
+
+            <motion.div
+              animate={{opacity: [1, 0.8, 1]}}
+              transition={{ease: "easeInOut", duration:2, loop: 4}}
+            >
+              <img src={pause} alt="pause"></img>
+            </motion.div>
+            
             <motion.div className="container pt-3 d-flex justify-content-center">
               <Link to="/">
                 <Button>Wróc do Menu</Button>
