068922e09acae059f3030ed78c4574730383e5d5
Jakub Chabrowski <chabrowski.kuba@gmail.com>
2020-07-09 18:37:23 +0200

    testing timer

--- src/components/Animations/TestAnimations/ExhaleAnimation.js
+++ src/components/Animations/TestAnimations/ExhaleAnimation.js
@@ -3,19 +3,23 @@ import { motion } from 'framer-motion';
 import { Paragraph }  from '../../../styles/Styles';
 import Timer from 'react-compound-timer';
 import { Button, ParagraphSmaller } from '../../../styles/Styles';
-import { pluralize } from 'pluralize';
+// import { pluralize } from 'pluralize';
 
 // const pluralize = require('pluralize');
 
 const ExhaleAnimation = () => {
 
   const [message, setMessage] = useState();
+  const [isTimerOn, setTimer] = useState();
 
   useEffect(() => {
     let safetyInfo = <ParagraphSmaller>Jeżeli poczuje się Pan/i słabo należy przerwać próbę.</ParagraphSmaller>;
     let message = safetyInfo;
     setMessage(message);
+    let isTimerOn = true;
+    setTimer(isTimerOn)
     }, []);
+  
 
     let stopTimer = () => {
       let message = <><ParagraphSmaller>Dziękujemy za wykonanie Testu!</ParagraphSmaller><ParagraphSmaller>Pana/i Wynik to <Timer.Seconds /> sekund!</ParagraphSmaller></>
@@ -25,6 +29,8 @@ const ExhaleAnimation = () => {
     let maxBreatheLength = () => {
       let message = <ParagraphSmaller>Dziękujemy za wykonanie Testu! Udało się Panu/i wydmuchiwać powietrze przez 45 sekund!</ParagraphSmaller>
       setMessage(message);
+      let isTimerOn = false;
+      setTimer(isTimerOn);
     } 
   
 
@@ -38,27 +44,27 @@ const ExhaleAnimation = () => {
       </motion.div>
         <Timer 
           checkpoints={[
-            {time: 45000,
+            {time: 5000,
             callback: () =>  maxBreatheLength()}
           ]}
-          onStop = { () => stopTimer()}>
-          {({stop}) => (
+          onStop = {() => stopTimer()}>
+          {({stop, getTimerState, getTime}) => (
           <React.Fragment>
                 <motion.div
                   animate={{opacity: [0, 1]}}
                   transition={{ease: "easeInOut", duration: 2}}>
-            
+                    <div>{getTimerState()} {getTime()}</div>
+                    {isTimerOn?
                     <Timer.Seconds />
+                    : null} 
                     <br></br>
                     <Button onClick={stop}>Stop</Button>
+                  </motion.div>
 
-                  </motion.div>               
                   <motion.div
                     animate={{opacity: [0, 1]}}
-                    transition={{ease: "easeIn", duration: 2, delay: 2}}>
-                      
+                    transition={{ease: "easeIn", duration: 2, delay: 2}}>    
                       {message ? message : null}
-
                   </motion.div>
           </React.Fragment>
           )}
